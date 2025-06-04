8d8ddc3ef94db26a2a90b630af7e70694a244b14
gimjin <kimseongrim@gmail.com>
2025-06-04 10:02:41 +0000

    添加 project1

---
+++ project1/eslint.config.js
@@ -0,0 +1,10 @@
+import pluginVue from 'eslint-plugin-vue'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default [
+  ...pluginVue.configs['flat/recommended'],
+  stylistic.configs['recommended-flat'],
+  {
+    ignores: ['dist/*'],
+  },
+]
