d8a3944d4c04225dff8ca629f91a56342bf6f177
gimjin <kimseongrim@gmail.com>
2025-06-11 06:30:59 +0000

    修改 project1 配置

--- project1/eslint.config.js
+++ project1/eslint.config.js
@@ -1,10 +1,28 @@
+import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
-import stylistic from '@stylistic/eslint-plugin'
+import pluginPrettier from 'eslint-plugin-prettier'
+import configPrettier from 'eslint-config-prettier'
 
 export default [
+  // 基础 JavaScript 规则
+  js.configs.recommended,
+  // Vue 推荐规则
   ...pluginVue.configs['flat/recommended'],
-  stylistic.configs.recommended,
+  // Prettier 配置
+  configPrettier,
   {
-    ignores: ['dist/*'],
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+    ignores: [
+      'dist/**/*',
+      'node_modules/**/*',
+      '**/*.min.css',
+      'build/**/*',
+      '.git/**/*',
+    ],
   },
 ]
